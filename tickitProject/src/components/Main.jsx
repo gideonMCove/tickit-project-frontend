cc8101255be21f156965a5f24231ac58b10d3773
GideonCove <gideoncove@gmail.com>
2024-07-15 23:29:51 -0400

    Event and Venue detail pages created, routes set up

--- tickitProject/src/components/Main.jsx
+++ tickitProject/src/components/Main.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes} from 'react-router-dom'
 import Home from './Home'
+import EventDetail from './EventDetail'
+import VenueDetail from './VenueDetail'
 
 const Main = () => {
     return(
         <div className="Main">
             <Routes>
                 <Route path="/" element = {<Home />}/>
+                <Route path="/events/detail/:eventId" element={<EventDetail />} />
+                <Route path="/venue/detail/:venueId" element={<VenueDetail />} />
             </Routes>
         </div>
     )
