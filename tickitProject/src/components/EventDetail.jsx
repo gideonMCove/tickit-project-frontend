40040e8a3fe212ce28ba7e69b9f114ff677ea625
GideonCove <gideoncove@gmail.com>
2024-07-16 16:01:32 -0400

    data displayed

--- tickitProject/src/components/EventDetail.jsx
+++ tickitProject/src/components/EventDetail.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
+// need use params, change 0 to equal id
 export default function EventDetail () {
-    const [details,setDetails] = useState([])
+    const [details,setDetails] = useState([""])
+    let { eventId } = useParams()
 
     useEffect(() =>{
 
@@ -11,7 +13,7 @@ export default function EventDetail () {
                 try {
                     const response = await axios.get('http://localhost:8000/events/')
                     setDetails(response)
-                    console.log('response', response)
+                    
                 } catch (error) {
                     console.error('Cannot load details', error)
                 }
@@ -19,12 +21,28 @@ export default function EventDetail () {
             }
             getDetail()
         },[])
+        console.log('response', details.data)
+        console.log('details', details)
+        if (details != ""){
+            console.log('artist', details.data[0].artist)
+        }
+        
+        // console.log('details.artist', details.data[0].artist)
     return (
         <div className = "detailPage">
             
                 <h1>EventDetail!</h1>
                 {
-                    <h1>{details.genre}</h1>
+                    details != "" ? (
+                    <h1>
+                        {details.data[0].artist}<br />
+                        Genre: {details.data[0].genre}<br />
+                        Ticket Price: ${details.data[0].price}<br />
+                        Ticket Limit: {details.data[0].ticket_limit}
+                        </h1>
+                    ) : (
+                        <h1>Data is not loaded</h1>
+                    )
                 }
             
         </div>
